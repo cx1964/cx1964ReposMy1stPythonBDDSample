87bfebf900a1af4f0e09bad156d297bfb5490282
cx1964 <cx1964@gmail.com>
2021-02-27 21:09:11 +0100

    Update 01_install.sh
    pas documentatie aan

--- 2framework_voorbeelden/01_pytest-bdd/01_install.sh
+++ 2framework_voorbeelden/01_pytest-bdd/01_install.sh
@@ -1,6 +1,8 @@
 # file: install.sh
 # functie: install omgeving in deze subdirectory
 # opmerking: zie ook ./README.md
+#            Voor werking pipenv zie
+#            https://thoughtbot.com/blog/how-to-manage-your-python-projects-with-pipenv
 
 # Install pipenv in Ubuntu als die nog niet bestaat
 sudo apt install pipenv
