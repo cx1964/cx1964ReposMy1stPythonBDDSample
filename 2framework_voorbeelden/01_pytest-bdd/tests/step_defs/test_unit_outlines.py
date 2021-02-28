6056fcd2629c864103026e4cb8e1b4bf973cd422
cx1964 <cx1964@gmail.com>
2021-02-28 09:50:01 +0100

    test tbv cucumbers.py
    test tbv cucumbers.py

--- 2framework_voorbeelden/01_pytest-bdd/tests/step_defs/test_unit_outlines.py
+++ 2framework_voorbeelden/01_pytest-bdd/tests/step_defs/test_unit_outlines.py
@@ -9,7 +9,6 @@ from pytest_bdd import scenarios, given, when, then
 
 
 # Scenarios
-
 CONVERTERS = {
     'initial': int,
     'some': int,
@@ -20,14 +19,12 @@ scenarios('../features/unit_outlines.feature', example_converters=CONVERTERS)
 
 
 # Given Steps
-
 @given('the basket has "<initial>" cucumbers')
 def basket(initial):
     return CucumberBasket(initial_count=initial)
 
 
 # When Steps
-
 @when('"<some>" cucumbers are added to the basket')
 def add_cucumbers(basket, some):
     basket.add(some)
@@ -39,7 +36,6 @@ def remove_cucumbers(basket, some):
 
 
 # Then Steps
-
 @then('the basket contains "<total>" cucumbers')
 @then('the basket contains "<leftover>" cucumbers')
 def basket_has_total(basket, total):
