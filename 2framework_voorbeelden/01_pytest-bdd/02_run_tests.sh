9a880e309a865ecdf79d8074b245a42111fe7d35
cx1964 <cx1964@mail.com>
2021-02-28 16:48:32 +0100

    tODo 02a

--- 2framework_voorbeelden/01_pytest-bdd/02_run_tests.sh
+++ 2framework_voorbeelden/01_pytest-bdd/02_run_tests.sh
@@ -1,16 +1,9 @@
-# file: 02_run_test.sh
+# file: 02a_run_test.sh
 # functie: Voorbeeld hoe men python scripts kan draaien waarvan de benodigde modules mbv Pipenv zijn geinstalleerd
 # opmerking: dit script werkt alleen als eerts het script ./01_install.sh is gedraait
 
-# start de python Pipenv
-pipenv shell
+# run de specifieke test test_unit_basic.py
+pipenv python run pytest tests/step_defs/test_unit_basic.py
 
-# run nu de tests
-# bv de specifike test test_unit_basic.py
-pytest tests/step_defs/test_unit_basic.py
-
-# run alle gedefinieerde tests
-pytest
-
-# verlaat de environment
-exit
+# run alle tests
+pipenv python run pytest
---
+++ 2framework_voorbeelden/01_pytest-bdd/02a_run_tests.werk_nog_niet.sh
@@ -0,0 +1,12 @@
+# file: 02a_run_test.sh
+# functie: Voorbeeld hoe men python scripts kan draaien waarvan de benodigde modules mbv Pipenv zijn geinstalleerd
+# opmerking: dit script werkt alleen als eerts het script ./01_install.sh is gedraait
+
+# zie https://stackoverflow.com/questions/51968643/how-to-use-pipenv-in-python-shell-script
+
+# run nu de tests
+# bv de specifike test test_unit_basic.py
+pipenv run python3 pytest tests/step_defs/test_unit_basic.py
+
+# run alle gedefinieerde tests
+pipenv run python3 pytest
