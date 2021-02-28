db1e545d666aae07ab623f0f2c581b330828e57a
cx1964 <cx1964@mail.com>
2021-02-28 16:52:27 +0100

    aanpassing

--- 2framework_voorbeelden/01_pytest-bdd/02_run_tests.sh
+++ 2framework_voorbeelden/01_pytest-bdd/02_run_tests.sh
@@ -1,9 +1,11 @@
-# file: 02a_run_test.sh
+# file: 02_run_test.sh
 # functie: Voorbeeld hoe men python scripts kan draaien waarvan de benodigde modules mbv Pipenv zijn geinstalleerd
 # opmerking: dit script werkt alleen als eerts het script ./01_install.sh is gedraait
 
+pipenv shell
+
 # run de specifieke test test_unit_basic.py
-pipenv python run pytest tests/step_defs/test_unit_basic.py
+pytest tests/step_defs/test_unit_basic.py
 
 # run alle tests
-pipenv python run pytest
+pytest
